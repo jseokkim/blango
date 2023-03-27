2ac85569d7fd39c86acf790f349a74497588570a
Jaeseok Kim <jseokkim@hotmail.com>
2023-03-27 07:41:24 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -70,6 +70,7 @@ class Dev(Configuration):
         "allauth.socialaccount.providers.google", 
         "rest_framework", 
         "rest_framework.authtoken", 
+        "drf_yasg", 
     ]
 
     MIDDLEWARE = [
@@ -229,6 +230,13 @@ class Dev(Configuration):
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.IsAuthenticatedOrReadOnly"
         ],
+    }
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
     }
 
 
