93046b47391dea17aab5824d1cddeede18643942
Jaeseok Kim <jseokkim@hotmail.com>
2023-03-27 23:01:52 +0000

    Finish viewsets and routers

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -59,3 +59,8 @@ class PostDetailSerializer(PostSerializer):
 
         return instance
 
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
